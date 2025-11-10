0eaf7d8406cd76196029e4fa538b2f5058701438
Delfi2Avalos <delfiavalos35@gmail.com>
2025-11-10 19:36:22 -0300

    modificaciones

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, me } from "../controllers/auth.controller.js";
+import { requireAuth } from "../middlewares/auth.js";
 
 const r = Router();
 
-// registro y login
 r.post("/register", register);
 r.post("/login", login);
+r.get("/me", requireAuth(), me); // devuelve perfil del usuario autenticado
 
 export default r;

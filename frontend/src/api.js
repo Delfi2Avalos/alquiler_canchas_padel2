009fc60512a89ea0314ecbaf0c50d10025c2d7f3
Delfi2Avalos <delfiavalos35@gmail.com>
2025-11-22 20:03:26 -0300

    modificacion para q cualquier persona pueda ingresar a la pagina

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 // Crear instancia de axios
-const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  const api = axios.create({
+  baseURL: "http://192.168.1.41:3001/api",
+  // baseURL: "http://192.168.0.105:3001/api", //wifi facultad: Instituto Tecnologico 1
   headers: {
     "Content-Type": "application/json",
   },
